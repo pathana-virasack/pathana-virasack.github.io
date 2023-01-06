d5efb5ece2372eb7a26ad6aa93f9f1a649ba604b
Pathana <p.virasack@gmail.com>
2023-01-06 12:40:21 +0100

    rajout media container et hover sur media button

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const txtTitleAnim = document.querySelector("h1");
+new Typewriter(txtTitleAnim, { 
+    // loop: true,   
+  deleteSpeed:60
+})
+  .changeDelay(60)
+  .typeString("Bonjour moi c'est Pathana")
+  .pauseFor(300)
+  .typeString("<strong>, Dev Full-Stack</strong> !")
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString( "<strong><span style='color: #ff6910'> HTML</span> </strong> !")  
+  .pauseFor(1000)
+  .deleteChars(7)
+  .typeString("<strong><span style='color: #2965f1'> CSS</span> </strong> !")
+  .pauseFor(1000)
+  .deleteChars(6)
+  .typeString(
+    "<strong><span style='color: #f0db4f'> JAVASCRIPT</span> </strong> !"
+  )
+.start()
